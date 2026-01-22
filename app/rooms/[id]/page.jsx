8e409d5e6975d8ddf6b742e8ae6c3e6facaec6d1
Annie Huang <annie.hn@gmail.com>
2026-01-22 21:14:19 +1100

    Filter room from rooms list from the data json and if the room is not found, return Room Not Found <Heding>

--- app/rooms/[id]/page.jsx
+++ app/rooms/[id]/page.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import rooms from '@/data/rooms.json';
+import Heading from '@/components/Heading';
 
 const RoomPage = ({ params }) => {
   const { id } = params;
+  const room = rooms.find((room) => room.$id === id);
+
+  if (!room) {
+    return <Heading title='Room Not Found' />;
+  }
+
   return <div>Room {id}</div>;
 };
 
