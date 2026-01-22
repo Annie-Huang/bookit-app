1c7824deba4894dbb58aaa5bb076805a34263430
Annie Huang <annie.hn@gmail.com>
2026-01-22 21:00:47 +1100

    Use dynamic title props

--- components/Heading.jsx
+++ components/Heading.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Heading = () => {
+const Heading = ({ title }) => {
   return (
     <section className='bg-white mb-5 shadow px-4 py-4'>
       <h1 className='text-2xl font-bold tracking-tight text-gray-900'>
-        Available Rooms
+        {title}
       </h1>
     </section>
   );
