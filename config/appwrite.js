daba084a21a2ec71d97b9b0b6cd5229e05b44d9f
Annie Huang <annie.hn@gmail.com>
2026-01-24 20:24:56 +1100

    Create createSessionClient method and also export both methods in appwrite.js

--- config/appwrite.js
+++ config/appwrite.js
@@ -21,4 +21,23 @@ const createAdminClient = async () => {
   };
 };
 
-const createSessionClient = async (session) => {};
+const createSessionClient = async (session) => {
+  const client = new Client()
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT) // Your API Endpoint
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT); // Your project ID
+
+  if (session) {
+    client.setSession(session);
+  }
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+  };
+};
+
+export { createAdminClient, createSessionClient };
